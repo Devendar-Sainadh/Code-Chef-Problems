157d850e08ecc499658f342b2d52dffae827c66c
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-19 22:50:52 +0530

    Create EQCARDGAME.cpp

---
+++ CodeChef Starters 22 Division 3 (Rated)/EQCARDGAME.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int k;
+    cin>>k;
+    int x=52%k;
+    cout<<x<<endl;
+}
+    
+    return 0;
+}
