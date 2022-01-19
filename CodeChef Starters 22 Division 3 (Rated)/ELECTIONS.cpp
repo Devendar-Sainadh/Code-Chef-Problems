081c360c2c569c4eb9a6df2483b4c9dd3df98057
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-19 22:51:42 +0530

    Create ELECTIONS.cpp

---
+++ CodeChef Starters 22 Division 3 (Rated)/ELECTIONS.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int A,B,C;
+    cin>>A>>B>>C;
+    if(A+B+C==101){
+    if(A>50){
+        cout<<"A"<<endl;
+    }
+    else if(B>50){
+        cout<<"B"<<endl;
+    }
+    else if(C>50){
+        cout<<"C"<<endl;
+    }
+    else{
+        cout<<"NOTA"<<endl;
+    }
+    }
+}
+   
+    return 0;
+}
