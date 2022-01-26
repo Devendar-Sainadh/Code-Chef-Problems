24885156a953ebe41d999d7461f50a7da3a6cc11
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-26 21:43:56 +0530

    Create CHFPROFIT.cpp

---
+++ CodeChef Starters 23 Division 3 (Rated)/CHFPROFIT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int x,y,z,p;
+    cin>>x>>y>>z;
+    p=(x*z)-(x*y);
+    cout<<p<<endl;
+}
+    
+    return 0;
+}
