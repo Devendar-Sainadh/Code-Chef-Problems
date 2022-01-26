ff4f49347617f7e047e9cf52bd2b34a6960c6ed0
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-26 21:44:31 +0530

    Create AVGOF3.cpp

---
+++ CodeChef Starters 23 Division 3 (Rated)/AVGOF3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int x;
+    cin>>x;
+    int p=1;
+    int q=2;
+    int r;
+    r=3*x-3;
+    cout<<p<<" "<<q<<" "<<r<<endl;
+
+}
+    
+    return 0;
+}
