8781318363e648943cd6c2a8449dd2a2b981e4a6
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-02-02 21:45:59 +0530

    Create BADMINTON.cpp

---
+++ CodeChef Starters 24 Division 3 (Rated)/BADMINTON.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int p;
+    cin>>p;
+    
+    cout<<(p/2)+1<<endl;
+    
+}
+    
+    return 0;
+}
