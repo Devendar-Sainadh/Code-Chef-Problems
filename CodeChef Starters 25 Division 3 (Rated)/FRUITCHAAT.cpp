fa764f6c0f2e10cfc047661212243939a9b8731f
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-02-09 21:42:28 +0530

    Create FRUITCHAAT.cpp

---
+++ CodeChef Starters 25 Division 3 (Rated)/FRUITCHAAT.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int x,y;
+    cin>>x>>y;
+    if((x/2)<=y){
+        cout<<x/2<<endl;
+    }
+    else{
+        cout<<y<<endl;
+    }
+    
+}
+    
+    return 0;
+}
