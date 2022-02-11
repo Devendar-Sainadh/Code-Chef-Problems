db979fe1d17112d87c2a212e6da9336c4b9f58d2
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-02-11 16:24:53 +0530

    Create HELIUM.cpp

---
+++ February Long 2022 - II, Division 3 (Rated)/HELIUM.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int a,b,x,y;
+    cin>>a>>b>>x>>y;
+    if((x*y)>=(a*b)){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+    
+    return 0;
+}
