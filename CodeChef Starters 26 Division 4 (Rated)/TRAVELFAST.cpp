c04c089a5f22d3a1c33ea6161590019ec33f00f8
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-02-17 16:30:01 +0530

    Create TRAVELFAST.cpp

---
+++ CodeChef Starters 26 Division 4 (Rated)/TRAVELFAST.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int x,y;
+    cin>>x>>y;
+    if(x<y){
+        cout<<"BIKE"<<endl;
+    }
+    else if(y<x){
+        cout<<"CAR"<<endl;
+    }
+    else{
+        cout<<"SAME"<<endl;
+    }
+}
+    
+    return 0;
+}
