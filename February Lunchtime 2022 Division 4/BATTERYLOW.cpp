5911c38cc519b7d29e402186b9600fe357f91b28
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-03-01 23:13:20 +0530

    Create BATTERYLOW.cpp

---
+++ February Lunchtime 2022 Division 4/BATTERYLOW.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int T;
+cin>>T;
+while(T--){
+    int x;
+    cin>>x;
+    if(x<=15)
+    cout<<"Yes"<<endl;
+    else{
+        cout<<"No"<<endl;
+    }
+}
+    
+    return 0;
+}
