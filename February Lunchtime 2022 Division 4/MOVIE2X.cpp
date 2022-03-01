bde6e348289585b4b0b9eda252f4646e1d8d9ed2
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-03-01 23:14:56 +0530

    Create MOVIE2X.cpp

---
+++ February Lunchtime 2022 Division 4/MOVIE2X.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+
+    long long int x,y;
+    cin>>x>>y;
+    
+    long long int a=y/2;
+    long long int b=x-y;
+    
+    cout<<a+b<<endl;
+
+    return 0;
+}
